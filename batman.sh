29c4899ae355db1fa37dcaa84373bae54af71e4f
dorin98 <doringeman@gmail.com>
2018-09-07 17:29:04 +0300

    LE4HS

--- batman.sh
+++ batman.sh
@@ -1,8 +1,8 @@
-#! /bin/bash
+#!/usr/bin/env bash
 
-trap "echo -n o" 1
+trap "echo -n l" 1
 trap "echo -n u" 2
-trap "echo -n c" 3
+trap "echo -n 4" 3
 trap "echo -n d" 4
 trap "echo -n e" 5
 trap "echo -n z" 6
@@ -11,7 +11,7 @@ trap "echo -n h" 8
 trap "echo -n \ " 10
 trap "echo -n r" 11
 trap "echo -n k" 12
-trap "echo -n l" 13
+trap "echo -n s" 13
 
 while [ 1 ]; do
     sleep 1
